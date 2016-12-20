418419a1445ccba1f1a06b47d6a3c7c6b9adb331
Полина <paulina.shevlyakova@yandex.ru>
2016-12-20 16:35:35 +0300

    добавила InfoAboutScope

---
+++ InfoAboutScope.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Antlr.Runtime;
+using Antlr.Runtime.Tree;
+
+namespace MathLang
+{
+    public class InfoAboutScope : NodeData
+    {
+        private Scope localScope;
+        private IdentDescription ident;
+
+        public InfoAboutScope()
+            : base()
+        {
+        }
+        public InfoAboutScope(IToken t)
+            : base(t)
+        {
+        }
+        public InfoAboutScope(CommonTree node)
+            : base(node)
+        {
+        }
+
+        public Scope LocalScope
+        {
+            get { return localScope; }
+            set { localScope = value; }
+        }
+        public IdentDescription Ident
+        {
+            get { return ident; }
+            set { ident = value; }
+        }
+    }
+}
