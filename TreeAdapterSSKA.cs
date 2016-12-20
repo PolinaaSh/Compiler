6d3d267b4469be91535d9044091c5e410a05bdd2
Полина <paulina.shevlyakova@yandex.ru>
2016-12-20 16:58:42 +0300

    Добавила TreeAdapter

---
+++ TreeAdapterSSKA.cs
@@ -0,0 +1,13 @@
+﻿using Antlr.Runtime;
+using Antlr.Runtime.Tree;
+
+namespace MathLang
+{
+    public class TreeAdapterSSKA : CommonTreeAdaptor
+    {
+        public override object Create(IToken payload)
+        {
+            return new NodeData(payload);
+        }
+    }
+}
